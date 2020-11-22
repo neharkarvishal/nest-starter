4a94b06b83dcd26be99c185e2888dd4db8d16c00
Vishal <neharkar.vishal@gmail.com>
2020-11-22 18:53:17 +0530

    formatting base code, set line length to 82

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+const API_DEFAULT_PORT = 3000
+const API_DEFAULT_PREFIX = '/api/v1'
+
+const SWAGGER_TITLE = 'API'
+const SWAGGER_DESCRIPTION = 'API'
+const SWAGGER_PREFIX = '/docs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap().then((_) => _)
